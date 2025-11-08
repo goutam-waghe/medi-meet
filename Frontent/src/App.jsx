18f387beb5697eaa0a7e927114bf602b55a8e6df
goutam-waghe <goutamwaghe@gmail.com>
2025-11-08 14:04:56 +0530

    setting up thing

---
+++ Frontent/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <h className="bg-amber-600">Our meedimeet app</h>
+    </>
+  )
+}
+
+export default App
---
+++ Frontent/src/components/login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const login = () => {
+  return (
+    <div>
+        <form action="">
+            
+        </form>
+    </div>
+  )
+}
+
+export default login
